cbfa71ce5b2e49903e936c4207aadf3b64c94b0e
Alfonso Acosta <fons@weave.works>
2016-03-07 12:44:12 +0000

    Replace jq with --query when possible

--- aws/ecs/packer/to-upload/peers.sh
+++ aws/ecs/packer/to-upload/peers.sh
@@ -7,24 +7,20 @@ aws=/usr/local/bin/aws
 export AWS_DEFAULT_REGION=$(curl -s curl http://169.254.169.254/latest/dynamic/instance-identity/document | \
                             jq -r .region)
 current_instance_id=$(curl -s curl http://169.254.169.254/latest/meta-data/instance-id)
-current_autoscaling_group=$($aws autoscaling describe-auto-scaling-instances | \
-                            jq -r ".AutoScalingInstances[] | select(.InstanceId == \"$current_instance_id\") | .AutoScalingGroupName")
+current_autoscaling_group=$($aws autoscaling describe-auto-scaling-instances --query "AutoScalingInstances[?InstanceId==\`$current_instance_id\`].AutoScalingGroupName"  --output text)
 
 if [ -z "$current_autoscaling_group" ]; then
   >&2 echo "instance doesn't belong to an autoscaling group"
   exit 1
 fi
 
-peer_instances=$($aws autoscaling describe-auto-scaling-instances | \
-                jq -r ".AutoScalingInstances[] | select(.AutoScalingGroupName == \"$current_autoscaling_group\" and .InstanceId != \"$current_instance_id\") | .InstanceId")
+peer_instances=$($aws autoscaling describe-auto-scaling-instances --query "AutoScalingInstances[? AutoScalingGroupName==\`$current_autoscaling_group\` && InstanceId!=\`$current_instance_id\`].InstanceId" --output text)
 
 if [ -z "$peer_instances" ]; then
 # no peers found
   exit 0
 fi
 
-peer_ips=$($aws ec2 describe-instances --instance-ids $peer_instances | \
-	jq -r '.Reservations | .[] | .Instances | .[] | .NetworkInterfaces | .[] | .PrivateIpAddress')
+peer_ips=$($aws ec2 describe-instances --instance-ids $peer_instances --query 'Reservations[].Instances[].NetworkInterfaces[].PrivateIpAddress' --output text)
+echo $peer_ips
 
-echo $peer_ips | tr '\n' ' '
-echo
